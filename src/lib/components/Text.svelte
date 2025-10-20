25b86e4b37dd9c3507c486214f0f7a03fb716fd9
Phillip Malboeuf <phil@phils.computer>
2025-10-20 11:03:51 -0400

    margins fix

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -185,9 +185,13 @@
       border-spacing: $s0;
       margin: 0 calc($s0 * -1) calc($s0 * -1);
 
+      @media (max-width: $tablet_portrait) {
+        margin: 0 calc($s-2 * -1) calc($s-2 * -1);
+        max-width: 100%;
+      }
+
       @media (max-width: $mobile) {
         border-spacing: $s-2;
-        margin: 0 calc($s-2 * -1) calc($s-2 * -1);
         display: block;
         max-width: 100%;
         overflow-x: auto;
